f5c5d3936f8410be84119c283cb728c352d191b9
Connor Filipovic <filipovicconnor@gmail.com>
2016-02-11 03:58:26 -0600

    update

--- cs101/projects/project2/project2.cpp
+++ cs101/projects/project2/project2.cpp
@@ -1,9 +1,14 @@
+//Connor Filipovic
+//Feb 8, 2016
+//Project 2
+//CS101 Dixon
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cctype>
-#include <sstream>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 class object {
@@ -13,7 +18,11 @@ class object {
     vector<vector<string> > data;
 };
 
-void quicksort(object& data, int left, int right);
+vector<vector<string>> merge_sort(object& obj, int col);
+vector<string> merge(const vector<int>& left, const vector<int>& right, int col);
+void generalsort(object& data, object& sort);
+void reverse(object&data);
+void quicksort(object& data, int left, int right, int col);
 int getCols(string fileName);
 int getRows(string fileName);
 int findColumn(object, string name);
@@ -39,6 +48,7 @@ int main(int argc, char** argv) {
     for(int d = 0; d < data.rows; d++) {
 	data.data[d].resize(data.cols);
     }
+
     input1.close();
     input1.open(argv[1]);
 
@@ -78,7 +88,6 @@ int main(int argc, char** argv) {
     input2.open(argv[2]);
     x = 0;
     y = 0;
-    //process all the data from the 2nd input file
     while(!input2.eof()) {
 	string line;
 	getline(input2, line);
@@ -96,17 +105,11 @@ int main(int argc, char** argv) {
 
     //close the 2nd file
     input2.close();
-    for(int g = 1; g < data.rows-1; g++) {
-	for(int h = 0; h < data.cols; h++) {
-	    cout << data.data[g][h] << ",";
-	}
-	cout << endl;
-    }
-  
-    //sort the data
-    quicksort(data, 1, data.rows-2);
 
-    for(int g = 1; g < data.rows-1; g++) {
+    //sort the data
+    generalsort(data, sort);
+
+    for(int g = 1; g < data.rows; g++) {
 	for(int h = 0; h < data.cols; h++) {
 	    cout << data.data[g][h] << ",";
 	}
@@ -115,14 +118,31 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void quicksort(object& data, int left, int right) {
+void generalsort(object& data, object& sort) {
+    for(int x = 0; x < sort.rows; x++) {
+	//do the quicksort
+cout << sort.data[x][0] << endl;
+	quicksort(data, 1, data.rows-1, findColumn(data, sort.data[x][0]));
 
+	//if its descending reverse the data
+/*	if(sort.data[x][2].compare("descending") == 0) {
+	    reverse(data);
+	}
+  */  }
+}
 
+void reverse(object& data) {
+    for(int x = 0; x < data.rows; x++) {
+	vector<string> temp = data.data[x];
+	data.data[x] = data.data[(data.rows-1) - x];
+	data.data[(data.rows-1) - x] = temp;
+    }
+    
+    return;
+}	
+
+void quicksort(object& data, int left, int right, int col) {
 //find how to switch the entire row, not just the specify column
-
-
-
-int col = 0;
     int i = left;
     int j = right;
     vector<string> tmp;
@@ -141,12 +161,70 @@ int col = 0;
     }
 
     if(left < j) 
-	quicksort(data, left, j);
+	quicksort(data, left, j, col);
     if(i < right)
-	quicksort(data, i, right);
+	quicksort(data, i, right, col);
 
     return;
 }
+
+vector<string> merge_sort(vector<vector<string> >& obj, int col)  {  
+    // Termination condition: List is completely sorted if it  
+    // only contains a single element.  
+    if(obj.data.size() == 1)  
+    {  
+        return obj.data[0];  
+    }  
+  
+    // Determine the location of the middle element in the vector  
+    vector<string>::iterator middle = obj.begin() + (obj.size() / 2);  
+  
+    vector<vector<string> > left(obj.begin(), middle);  
+    vector<vector<string> > right(middle, obj.end());  
+  
+    // Perform a merge sort on the two smaller vectors  
+    left = merge_sort(left, col);  
+    right = merge_sort(right, col);  
+  
+    return merge(left, right, col);  
+} 
+
+vector<string> merge(const vector<string>& left, const vector<string>& right, int col)  {  
+    // Fill the resultant vector with sorted results from both vectors  
+    vector<string> result;  
+    unsigned left_it = 0, right_it = 0;  
+  
+    while(left_it < left.size() && right_it < right.size())  
+    {  
+        // If the left value is smaller than the right it goes next  
+        // into the resultant vector  
+        if(left[left_it][col] <= right[right_it][col])  
+        {  
+            result.push_back(left[left_it]);  
+            left_it++;  
+        }  
+        else  
+        {  
+            result.push_back(right[right_it]);  
+            right_it++;  
+        }  
+    }  
+  
+    // Push the remaining data from both vectors onto the resultant  
+    while(left_it < left.size())  
+    {  
+        result.push_back(left[left_it]);  
+        left_it++;  
+    }  
+  
+    while(right_it < right.size())  
+    {  
+        result.push_back(right[right_it]);  
+        right_it++;  
+    }  
+  
+    return result;  
+}  
 
 int findColumn(object data, string search) {
     for(int x = 0; x < data.cols; x++) {
@@ -164,32 +242,31 @@ int findColumn(object data, string search) {
 int getCols(string fileName) {
     int count = 0;
     
-    ifstream input1;
-    input1.open(fileName.c_str());
+    ifstream input;
+    input.open(fileName.c_str());
 
     string line;
-    getline(input1, line);
+    getline(input, line);
 
     string token;
     stringstream stream(line);
+
     while(getline(stream, token, ',')) {
 	count++;
     }
 
-    input1.close();
+    input.close();
     return count;	
 }
 
 int getRows(string fileName) {
     int count = 0;
 
+    string line;
     ifstream input;
     input.open(fileName.c_str());
-   
-    while(!input.eof()) {
-  	string line;
-	getline(input, line);
-
+  
+    while(getline(input, line)) { 
 	count++;
     } 
 
